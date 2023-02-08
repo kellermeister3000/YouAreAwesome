b4b1a1382de8e07a64503b32f671279bd197059c
Philip Keller <philipjoelkeller@gmail.com>
2023-02-07 22:39:50 -0800

    Ternary Operator - YouTube 1.10

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -31,11 +31,15 @@ struct ContentView: View {
                 let message1 = "You Are Awesome!"
                 let message2 = "You Are Great!"
                 
-                if messageString == message1 {
-                    messageString = message2
-                } else {
-                    messageString = message1
-                }
+//                if messageString == message1 {
+//                    messageString = message2
+//                } else {
+//                    messageString = message1
+//                }
+                
+                // Replaced with a swifty Ternary Operator
+                messageString = (messageString == message1 ? message2 : message1)
+                
             }
             .buttonStyle(.borderedProminent)
             
